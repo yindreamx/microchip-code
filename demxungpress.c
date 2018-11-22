6c0b9eff147021327581a880dc9568aa33b0c142
macOS <macOS@Admins-MacBook-Pro.local>
2018-11-22 10:06:44 +0700

    Create demxungpress.c

---
+++ demxungpress.c
@@ -0,0 +1,111 @@
+#include <REGX51.H>
+#include <stdio.h>
+#define LCD_rs P2_5
+#define LCD_rw P2_6
+#define LCD_en P2_7
+#define LCD_data P0
+#define ON P1_0
+
+void LCD_busy_delay(void)
+{
+	TH0 = 0x3C;
+	TL0 = 0xB0;
+	TR0 = 1;
+	while(!TF0){}
+	TR0 = 0;
+	TF0 = 0;
+}
+
+void LCD_cmd(unsigned char var)
+{
+	//Set cursor
+	LCD_rs = 0;
+	LCD_rw = 0;
+	LCD_data = var;
+	LCD_en = 1;
+	LCD_en = 0;
+	LCD_busy_delay();
+}
+
+void LCD_shift(var)
+{
+	LCD_rs = 0;
+	LCD_rw = 0;
+	LCD_data = var;
+	LCD_en = 1;
+	LCD_en = 0;
+	LCD_busy_delay();
+}
+
+void LCD_setup()
+{
+	//Function Set
+	LCD_cmd(0x38);
+	//Entry Mode Set
+	LCD_cmd(0x06);
+	//Display ON/OFF Control
+	LCD_cmd(0x0F);
+	//Clear Display
+	LCD_cmd(0x01);
+}
+
+void LCD_senddata(unsigned char var)
+{
+	LCD_rs = 1;
+	LCD_rw = 0;
+	LCD_data = var;
+	LCD_en = 1;
+	LCD_en = 0;
+	LCD_busy_delay();
+}
+
+void LCD_string(unsigned char *var)
+{
+	while(*var)
+		LCD_senddata(*var++);
+}
+
+int press()
+{
+	if (ON==0) //Nut duoc nhan
+	{ 	
+		LCD_busy_delay(); //Thoi gian de mach on dinh trang thai, bat 2 trang thai truoc va sau
+		if (ON!=0)
+		{
+			return 1;
+		}
+	}
+	else
+		return 0; //Khong co nut nhan
+	
+
+}
+ 
+void main()
+{
+	char str[10];
+	//int dem;
+	//unsigned char high, low;
+	unsigned int display;
+	TMOD = 0x51;
+	LCD_setup(); //LCD Setup
+	LCD_cmd(0x80); //Set cursor
+	LCD_string("Xung:");
+	TR1 = 1;
+
+	while(1)
+	{	if(press()==1)
+		{
+		TR1 = !TR1;
+		}
+		display = (int)TH1*256+(int)TL1;
+		//Gan doan text can hien thi vao string
+		sprintf(str,"%d",display);
+		LCD_cmd(0x87);
+		LCD_string(str);	
+		
+	}
+}
+
+
+
