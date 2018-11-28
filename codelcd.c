a683cdbd40757ba20bd9b5ad793365342c85ebb3
macOS <macOS@Admins-MacBook-Pro.local>
2018-11-28 14:33:09 +0700

    Update codelcd.c

--- codelcd.c
+++ codelcd.c
@@ -1,94 +1,112 @@
-#include <RX89X51.H>
-#define LCD_rs P2_5
-#define LCD_rw P2_6
-#define LCD_en P2_7
-#define LCD_data P0
-
-int LCD_busy_delay(int N)
+#include <REGX51.H>
+#define LCD_data P0 
+#define LCD_rs   P2_5 
+#define LCD_rw   P2_6 
+#define LCD_en   P2_7
+delay (int N)	
 {
-	unsigned char i,j;
-	for(i=1;i<N;i++)
-		for(j=1;j<255;j++);
+	int i,j;
+	for (i=0;i<=N;i++)
+		for (j=0;j<=125;j++);
 }
-void LCD_cmd(unsigned char var)
+void LCD_busy_delay(int N) 
+{          
+	unsigned char i,j;          
+	for(i=0;i<N;i++)                  
+		for(j=0;j<255;j++); 
+}
+void LCD_command(unsigned char var)
 {
-	//Set cursor
+	LCD_data = var;
 	LCD_rs = 0;
 	LCD_rw = 0;
-	LCD_data = var;
-	LCD_en = 1;
 	LCD_en = 0;
+	LCD_en = 1;
 	LCD_busy_delay(50);
 }
-void LCD_shift(var)
+void LCD_setup ()
 {
-	LCD_rs = 0;
-	LCD_rw = 0;
+	//function set
+	LCD_command(0x38);
+	//entry mode set
+	LCD_command(0x06);
+	//Display ON/OFF
+	LCD_command(0x0E);
+	//Clear display
+	LCD_command(0x01);
+	//Cursor display shift
+	//LCD_command(0x10);
+}
+void LCD_send (unsigned char var)
+{
 	LCD_data = var;
-	LCD_en = 1;
-	LCD_en = 0;
-	LCD_busy_delay(50);
-}
-void LCD_setup()
-{
-	//Function Set
-	LCD_cmd(0x38);
-	//Entry Mode Set
-	LCD_cmd(0x06);
-	//Display ON/OFF Control
-	LCD_cmd(0x0F);
-	//Clear Display
-	LCD_cmd(0x01);
-}
-void LCD_senddata(unsigned char var)
-{
 	LCD_rs = 1;
 	LCD_rw = 0;
-	LCD_data = var;
-	LCD_en = 1;
 	LCD_en = 0;
+	LCD_en = 1;
 	LCD_busy_delay(50);
 }
-void LCD_string(unsigned char *var)
+void LCD_string (unsigned char *var)
 {
-	while(*var)
-		LCD_senddata(*var++);
+	while (*var)	//still string end
+	LCD_send(*var++); //send char one by one
 }
-void LCD_build(unsigned char loc, unsigned char *msg) //Ham nap du lieu vao CGRAM
+void LCD_cursor (unsigned char var)
+{
+	LCD_data = var;
+	LCD_rs = 0;
+	LCD_rw = 0;
+	LCD_en = 0;
+	LCD_en = 1;
+	LCD_busy_delay(50);
+}
+void LCD_custom (unsigned char vitri, unsigned char *par)
 {
 	unsigned char i;
-	if (loc<8)
+	if(vitri<8)
 	{
-		LCD_cmd(0x40+loc*8);
+		LCD_command(0x40+(vitri*8));
 		for (i=0;i<8;i++)
-			LCD_senddata(msg[i]);
+		{
+			LCD_send(par[i]);
+		}
 	}
 }
- 
-void main()
+//void unsigned char ptr1[8]={0x00,0x04,0x02,0x1F,0x02,0x04,0x00,0x00};
+	 
+main ()
 {
-	char i;
-	//Khai bao mang cua custom character
-	unsigned char Custom_Char1[8] = {0x00,0x00,0x00,0x0E,0x0E,0x0E,0x00,0x00};
-	unsigned char Custom_Char2[8] = {0x00,0x00,0x04,0x0E,0x0E,0x0E,0x00,0x00};
-	unsigned char Custom_Char3[8] = {0x00,0x04,0x04,0x0E,0x0E,0x0E,0x00,0x00};
-	//LCD Setup
-	LCD_setup();
-	//Nap vao CGRAM
-	LCD_build(0,Custom_Char1[8]);
-	LCD_build(1,Custom_Char2[8]);
-	LCD_build(2,Custom_Char3[8]);
-	LCD_cmd(0x80); //Set cursor
-	LCD_string("HEHE"); //Send string 
-	while(1)
-	{
-		LCD_cmd(0xC9);
-		for(i=0;i<3;i++)
-			LCD_senddata(i);
-		for(i=2;i>=0;i--)
-			LCD_senddata(i);
-	}
+	 unsigned char i=0,add=0x80,t,vitri;
+	 unsigned char char1[8]={0x03,0x07,0x0D,0x0F,0x07,0x02,0x04,0x00};
+	 unsigned char char2[8]={0x18,0x1C,0x16,0x1E,0x1C,0x08,0x04,0x00};
+	 LCD_setup();
+	 LCD_custom(0,char1); 
+	 LCD_custom(1,char2);
+//	 for (i=0;i<8;i++)
+//	 {
+//	 	LCD_send(i);
+//	 }
+	 while (1)
+	 {
+	 	for (i=0;i<=15;i++)
+		{
+			vitri=add+i;
+			LCD_cursor(vitri);
+	 		LCD_send(0);
+	 		LCD_send(1);
+	 		delay(250);
+	 		LCD_command(0x01);
+			if (vitri==0x8F)
+			{
+				for (t=1;t<=14;t++)
+				{
+				LCD_cursor(vitri-t);
+	 			LCD_send(0);
+	 			LCD_send(1);
+	 			delay(250);
+	 			LCD_command(0x01);
+				}	
+	 		}
+		}
+	 }
 }
-
-
-
